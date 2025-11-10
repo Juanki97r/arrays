d218fb1603822d591197c15f6feceec8d495d656
Juanki97r <juanki97r@gmail.com>
2025-11-10 18:54:15 +0100

    funciona solo con el debuguer

--- src/main/java/daw/Funciones.java
+++ src/main/java/daw/Funciones.java
@@ -10,7 +10,9 @@ public class Funciones {
 
         char [] palabraChar = palabra.toCharArray();
 
-        for( int i =0, j = palabraChar.length; i< palabraChar.length && j == 0; i++ , j-- ){
+        for( int i =0, j = palabraChar.length; i<= palabraChar.length && j == 0; i++ , j-- ){
+
+            
 
             if(palabraChar[i] == palabraChar[j]){
                 control++; 
