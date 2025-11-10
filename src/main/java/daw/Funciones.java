331ce3bba74471ccf57cd3a86a730d941c6f991e
Juanki97r <juanki97r@gmail.com>
2025-11-10 17:54:56 +0100

    convertir String en un array de Char

---
+++ src/main/java/daw/Funciones.java
@@ -0,0 +1,23 @@
+package daw;
+
+public class Funciones {
+
+    public static int evaluaPalabra (String palabra){
+        
+        String palabraEv = "";
+
+        char [] palabraChar = palabraEv.toCharArray();
+
+        
+
+        
+
+
+
+
+
+        
+
+    }
+
+}
--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -1,7 +1,11 @@
 package daw;
 
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
         
+
+        String palabra = JOptionPane.showInputDialog("Introduce la palabra");
     }
 }
